bd35395e0854e52bc07a8317a1f760d11abb41fc
vladimir-dozorov <Vladimir.Dozorov@emergn.com>
2020-10-19 18:02:57 +0300

    added orm package and db context

---
+++ OrderManager/Services/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManager.Services
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=orders_local;Trusted_Connection=True;");
+        }
+    }
+}
